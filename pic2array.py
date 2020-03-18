0d7c95d03e8d1550f0f5d8fe490385ffc21f5d63
cyones <cristianyones@argeniss.com>
2020-03-18 20:36:58 -0300

    Main class

---
+++ pic2array.py
@@ -0,0 +1,17 @@
+from modules import VertexDetector, BoardExtractor, BoardInterpreter
+
+class Pic2Array():
+    def __init__(self, board_size):
+        self.vertex_detector = VertexDetector()
+        board_image_size = board_size * 2**4
+        self.board_extractor = BoardExtractor(board_image_size, board_image_size)
+        self.board_interpreter = BoardInterpreter()
+
+
+    def __call__(self, image):
+        vertexs, _ = self.vertex_detector(image)
+        board_image = self.board_extractor(image, vertexs)
+        board_configuration, is_ok = self.board_interpreter(board_image)
+        return board_configuration, is_ok, board_image
+
+        
