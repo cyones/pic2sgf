5ce1c373a89b2892a8022e7192dfb134aadaa512
Franco Victorio <victorio.franco@gmail.com>
2020-03-18 22:47:13 -0300

    Fix params path

--- modules/board_interpreter.py
+++ modules/board_interpreter.py
@@ -1,10 +1,13 @@
+from os import path
 from pic2sgf.models.cnn import CNN
 from torchvision import transforms
+
+params_path = path.join(path.dirname(__file__), '../models/parameters/cnn.pmt')
 
 class BoardInterpreter():
     def __init__(self):
         self.cnn = CNN()
-        self.cnn.load("models/parameters/cnn.pmt")
+        self.cnn.load(params_path)
         self.cnn.eval()
         self.to_tensor = transforms.ToTensor()
 
@@ -14,5 +17,3 @@ class BoardInterpreter():
         board = board.detach().numpy().round().astype(int)
         is_ok = (is_ok > 0.5).item()
         return board, is_ok
-        
-
