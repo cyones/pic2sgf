0a7153312f0b65041b772de541fa1815e2870d78
cyones <cristianyones@argeniss.com>
2020-04-09 18:41:53 -0300

    Rename files and folders

--- pic2array.py
+++ pic2array.py
@@ -1,10 +1,9 @@
-from .modules import CornerDetector, BoardExtractor, BoardInterpreter, BoardSizer
-
+from .src import CornerDetector, BoardExtractor, BoardInterpreter, BoardSizer
 IMG_SIZE = (512, 384)
 
 class Pic2Array():
     def __init__(self):
-        self.vertex_detector = CornerDetector()
+        self.corner_detector = CornerDetector()
         self.board_sizer = BoardSizer()
         self.board_extractor = {9 : BoardExtractor(9),
                                 19: BoardExtractor(19)}
@@ -12,7 +11,7 @@ class Pic2Array():
 
 
     def __call__(self, image):
-        corners = self.vertex_detector(image)
+        corners = self.corner_detector(image)
         board_size = self.board_sizer(image, corners)
         board_image = self.board_extractor[board_size](image, corners)
         board_configuration = self.board_interpreter(board_image)
