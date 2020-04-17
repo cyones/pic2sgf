73cfd9e36f9bf3460e6fede6ef49860816fb98d3
cyones <cristianyones@argeniss.com>
2020-04-17 11:34:35 -0300

    Improved board extraction

--- src/board_extractor.py
+++ src/board_extractor.py
@@ -3,38 +3,21 @@ from PIL import Image
 
 
 class BoardExtractor():
-    def __init__(self, board_size, pers_correction_iters = 20):
-        self.width = board_size * (2**4)
-        self.height = board_size * (2**4)
-        self.border = 1 / board_size
-        self.pers_correction_iters = pers_correction_iters
+    def __init__(self, board_size):
+        self.size = board_size * 16
+        B = np.array([[8, 8, 1],
+                      [self.size-8, 8, 1],
+                      [self.size-8, self.size-8, 1],
+                      [8, self.size-8, 1]]).T
+        self.T2 = np.linalg.inv(B[:, 0:3] * np.linalg.solve(B[:, 0:3], B[:, 3]))
 
-    def calc_depth(self, v):
-        z = np.array([1.0, 1.0, 1.0, 1.0])
-        fv = np.concatenate([v, z.reshape(4,1)], axis=1)
-        side = np.linalg.norm(v - np.roll(v, 1, axis=0), axis=1).mean()
-        for i in range(self.pers_correction_iters):
-            pre = np.linalg.norm(fv - np.roll(fv,  1, axis=0), axis=1)
-            pos = np.linalg.norm(fv - np.roll(fv, -1, axis=0), axis=1)
-            z *= 2 * side / (pre+pos)
-            fv = np.concatenate([v * z[:,np.newaxis], z.reshape(4,1)], axis=1)
-        return fv
-    
     def __call__(self, img, vertexs):
-        center = np.array([img.size[0]/2, img.size[1]/2])
-        vertexs = vertexs[:] - center
-        vertexs = self.calc_depth(vertexs)
-
-        board = Image.new('RGB', (self.width, self.height))
-        board_pix = board.load()
-        img_pix = img.load()
-        for j in range(self.height):
-            for i in range(self.width):
-                u = ((1.0 + self.border) * i / self.width) - self.border / 2
-                v = ((1.0 + self.border) * j / self.height) - self.border / 2
-                x = v * (u * vertexs[0] + (1-u) * vertexs[1]) + (1-v) * (u * vertexs[3] + (1-u) * vertexs[2])
-                x = x / x[2]
-                x = x[0:2] + center
-                x[0], x[1] = np.clip(x[0], 0, img.size[0]-1), np.clip(x[1], 0, img.size[1]-1)
-                board_pix[i,j] = img_pix[int(x[0]), int(x[1])]
-        return board
+        A = np.concatenate([vertexs.T, np.array([[1.0, 1.0, 1.0, 1.0]])], axis=0)
+        T1 = A[:, 0:3] * np.linalg.solve(A[:, 0:3], A[:, 3])
+        T = np.matmul(T1, self.T2)
+        T /= T[2,2]
+        board = img.transform((self.size, self.size),
+                              method=Image.PERSPECTIVE,
+                              data = T.reshape(-1),
+                              resample=Image.BILINEAR)
+        return board.transpose(Image.ROTATE_180)
