4b220864ad3e60a5f7a57b718a6dece287923a56
cyones <cristianyones@argeniss.com>
2020-04-18 23:29:32 -0300

    Improved interpreter (added augmentation)

--- models/interpreter.py
+++ models/interpreter.py
@@ -14,18 +14,24 @@ class iblock(nn.Module):
     def forward(self, x):
         return x + self.conv_path(x)
 
+def Pooling(in_dim, out_dim):
+    return nn.Sequential(nn.BatchNorm2d(in_dim), nn.ReLU(),
+                         nn.Conv2d(in_dim, out_dim, kernel_size=2, stride=2))
+
 class Interpreter(nn.Module):
     def __init__(self):
         super(Interpreter, self).__init__()
-        self.conv_blocks = nn.Sequential(nn.Conv2d(3, 8, kernel_size=2, stride=2), 
-                                         iblock(8), nn.ReLU(), nn.BatchNorm2d(8),
-                                         nn.Conv2d(8, 16, kernel_size=2, stride=2),
-                                         iblock(16), nn.ReLU(), nn.BatchNorm2d(16),
-                                         nn.Conv2d(16, 32, kernel_size=2, stride=2),
-                                         iblock(32), nn.ReLU(), nn.BatchNorm2d(32),
-                                         nn.Conv2d(32, 64, kernel_size=2, stride=2),
-                                         iblock(64), nn.ReLU(), nn.BatchNorm2d(64),
-                                         nn.Conv2d(64, 3, kernel_size=1))
+        self.conv_blocks = nn.Sequential(
+            nn.Conv2d(3, 8, kernel_size=2, stride=2),
+            iblock(8), iblock(8), Pooling(8, 16),
+            iblock(16), iblock(16), Pooling(16, 32),
+            iblock(32), iblock(32), Pooling(32, 64),
+            iblock(64), iblock(64),
+            nn.BatchNorm2d(64), nn.ReLU(), nn.Dropout2d(0.5), 
+            nn.Conv2d(64, 16, kernel_size=1),
+            nn.BatchNorm2d(16), nn.ReLU(), nn.Dropout2d(0.5), 
+            nn.Conv2d(16, 3, kernel_size=1),
+            )
 
     def forward(self, x):
         x = self.conv_blocks(x)
