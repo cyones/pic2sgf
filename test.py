f700b93c3e4eff1c8a60885116970191bfce1318
cyones <cristianyones@argeniss.com>
2020-04-24 14:26:28 -0300

    Updated test script

--- test.py
+++ test.py
@@ -1,20 +1,24 @@
 from pic2sgf import Pic2Array
-from PIL import Image
 from matplotlib import pyplot as plt
+from matplotlib.colors import Normalize
+from torchvision.datasets.folder import default_loader
+from tqdm import tqdm
+from PIL import Image
 
-pic2array = Pic2Array()
+pic2array = Pic2Array(1)
 
-plt.figure(figsize=(12, 8))
-for i in range(6):
-    image = Image.open(f'pic2sgf/test_images/{i+1}.jpg').resize((512, 384))
-    board, board_image = pic2array(image)
-    
-    plt.subplot(3, 4, 2*i+1)
+plt.figure(figsize=(20, 16))
+for i in tqdm(range(12)):
+    image = default_loader(f'pic2sgf/test_images/{i+1}.jpg')
+
+    configuration, report = pic2array(image)
+
+    plt.subplot(4, 6, 2*i+1)
     plt.imshow(image)
-    plt.subplot(3, 4, 2*i+2)
-    plt.imshow(board_image, extent=(0,1,0,1))
-    plt.imshow(board, extent=(0,1,0,1), alpha=0.5, cmap='bwr')
-    plt.title(f"Board size: {board.shape}")
+    plt.subplot(4, 6, 2*i+2)
+    plt.imshow(report['board_image'], extent=(0,1,0,1))
+    plt.imshow(configuration, extent=(0,1,0,1), alpha=0.25, cmap='bwr', norm = Normalize(vmin = -1, vmax = 1))
+
+print(report)
 
 plt.show()
-
