2c2513be809eb9a9db1a678b74852b0b7956b039
cyones <cristianyones@argeniss.com>
2020-05-13 18:07:56 -0300

    Improved sizer

--- models/sizer.py
+++ models/sizer.py
@@ -1,13 +1,12 @@
 import torch as tr
 from torch import nn
 
-
 class iblock(nn.Module):
     def __init__(self, dims):
         super(iblock, self).__init__()
-        self.conv_path = nn.Sequential(nn.BatchNorm2d(dims), nn.ReLU(),
+        self.conv_path = nn.Sequential(nn.BatchNorm2d(dims), nn.ELU(),
                                        nn.Conv2d(dims, dims, kernel_size=3, padding=1),
-                                       nn.ReLU(), nn.BatchNorm2d(dims),
+                                       nn.ELU(), nn.BatchNorm2d(dims),
                                        nn.Conv2d(dims, dims, kernel_size=3, padding=1))
         
     def forward(self, x):
@@ -17,32 +16,21 @@ class iblock(nn.Module):
 class Sizer(nn.Module):
     def __init__(self):
         super(Sizer, self).__init__()
-        self.conv_blocks = nn.Sequential(nn.Conv2d(3, 8, kernel_size=2, stride=2), 
-                                         iblock(8), iblock(8),
-                                         nn.ReLU(), nn.BatchNorm2d(8),
-                                         nn.Conv2d(8, 16, kernel_size=2, stride=2),
-                                         iblock(16), iblock(16),
-                                         nn.ReLU(), nn.BatchNorm2d(16),
-                                         nn.Conv2d(16, 32, kernel_size=2, stride=2),
-                                         iblock(32), iblock(32),
-                                         nn.ReLU(), nn.BatchNorm2d(32),
-                                         nn.Conv2d(32, 64, kernel_size=2, stride=2),
-                                         iblock(64), iblock(64),
-                                         nn.ReLU(), nn.BatchNorm2d(64),
-                                         nn.Conv2d(64, 8, kernel_size=1),
-                                         nn.ReLU(), nn.BatchNorm2d(8))
+        self.conv_blocks = nn.Sequential(nn.Conv2d(1, 3, kernel_size=2, stride=2), 
+                                         iblock(3), nn.ELU(), nn.BatchNorm2d(3),
+                                         nn.Conv2d(3, 8, kernel_size=2, stride=2), 
+                                         iblock(8), nn.ELU(), nn.BatchNorm2d(8),
+                                         nn.Conv2d(8, 16, kernel_size=2, stride=2), 
+                                         iblock(16), nn.ELU(), nn.BatchNorm2d(16),
+                                         nn.Conv2d(16, 32, kernel_size=2, stride=2), 
+                                         iblock(32), nn.ELU(), nn.BatchNorm2d(32),
+                                         nn.Conv2d(32, 3, kernel_size=1), nn.Sigmoid(),
+                                         nn.AdaptiveAvgPool2d(1))
                                          
-        self.out_layer = nn.Sequential(nn.Dropout(0.5),
-                                       nn.Linear(1536, 16),
-                                       nn.ReLU(), nn.BatchNorm1d(16),
-                                       nn.Dropout(0.5),
-                                       nn.Linear(16, 3))
 
     def forward(self, x):
         x = self.conv_blocks(x)
-        x = x.view(-1, 1536)
-        x = self.out_layer(x)
-        return x
+        return x.squeeze()
       
     def load(self, fname):
         self.load_state_dict(tr.load(fname, map_location=lambda storage, loc: storage))
--- src/board_sizer.py
+++ src/board_sizer.py
@@ -3,6 +3,8 @@ import numpy as np
 from pic2sgf.models import Sizer
 from torchvision.transforms import functional as ft
 
+from pic2sgf.src import BoardExtractor
+
 params_path = path.join(path.dirname(__file__), '../models/parameters/sizer.pmt')
 
 
@@ -12,11 +14,11 @@ class BoardSizer():
         self.cnn.load(params_path)
         self.cnn.eval()
         self.classes = [9, 13, 19]
+        self.board_extractor = BoardExtractor(8)
 
     def __call__(self, image, corners):
-        bbox = (corners[:,0].min(), corners[:,1].min(), corners[:,0].max(), corners[:,1].max()) 
-        image = image.crop(bbox).resize((256, 192))
-        x = ft.to_tensor(image).unsqueeze(0)
+        image, _ = self.board_extractor(image, corners)
+        x = ft.to_tensor(image).mean(0, keepdim=True).unsqueeze(0)
         probabilities = self.cnn(x).detach().numpy()
         board_size = probabilities.argmax()
         return self.classes[board_size], probabilities
