dce1591f34adcf7bac2b75f176baa3fecd39386a
cyones <cristianyones@argeniss.com>
2020-05-19 14:39:25 -0300

    Improved interpreter, raise error

--- models/interpreter.py
+++ models/interpreter.py
@@ -6,36 +6,42 @@ from torchvision import transforms
 class iblock(nn.Module):
     def __init__(self, dims):
         super(iblock, self).__init__()
-        self.conv_path = nn.Sequential(nn.BatchNorm2d(dims), nn.ReLU(),
+        self.conv_path = nn.Sequential(nn.BatchNorm2d(dims), nn.ELU(),
                                        nn.Conv2d(dims, dims, kernel_size=3, padding=1),
-                                       nn.ReLU(), nn.BatchNorm2d(dims),
+                                       nn.ELU(), nn.BatchNorm2d(dims),
                                        nn.Conv2d(dims, dims, kernel_size=3, padding=1))
         
     def forward(self, x):
         return x + self.conv_path(x)
 
 def Pooling(in_dim, out_dim):
-    return nn.Sequential(nn.BatchNorm2d(in_dim), nn.ReLU(),
+    return nn.Sequential(nn.BatchNorm2d(in_dim), nn.ELU(),
                          nn.Conv2d(in_dim, out_dim, kernel_size=2, stride=2))
 
 class Interpreter(nn.Module):
     def __init__(self):
         super(Interpreter, self).__init__()
         self.conv_blocks = nn.Sequential(
-            nn.Conv2d(3, 8, kernel_size=2, stride=2),
-            iblock(8), iblock(8), Pooling(8, 16),
-            iblock(16), iblock(16), Pooling(16, 32),
-            iblock(32), iblock(32), Pooling(32, 64),
-            iblock(64), iblock(64),
-            nn.BatchNorm2d(64), nn.ReLU(), nn.Dropout2d(0.5), 
-            nn.Conv2d(64, 16, kernel_size=1),
-            nn.BatchNorm2d(16), nn.ReLU(), nn.Dropout2d(0.5), 
-            nn.Conv2d(16, 3, kernel_size=1),
+            nn.Conv2d(3, 10, kernel_size=2, stride=2),
+            iblock(10), Pooling(10, 20),
+            iblock(20), Pooling(20, 40),
+            iblock(40), Pooling(40, 80),
+            iblock(80),
+            nn.ELU(), nn.BatchNorm2d(80), nn.Dropout2d(0.2),
+            nn.Conv2d(80, 20, kernel_size=1),
+            nn.ELU(), nn.BatchNorm2d(20)
             )
+        self.position = nn.Conv2d(20, 3, kernel_size=1)
+        self.wrong = nn.Sequential(
+            nn.Conv2d(20, 1, kernel_size=1),
+            nn.Sigmoid(), nn.AdaptiveMaxPool2d(1)
+        )
 
     def forward(self, x):
         x = self.conv_blocks(x)
-        return x
+        position = self.position(x)
+        wrong = self.wrong(x).squeeze(1).squeeze(1)
+        return position, wrong
       
     def load(self, fname):
         self.load_state_dict(tr.load(fname, map_location=lambda storage, loc: storage))
--- src/board_interpreter.py
+++ src/board_interpreter.py
@@ -1,6 +1,7 @@
 from os import path
 import numpy as np
-from pic2sgf.models import Interpreter
+from ..models import Interpreter
+from .exceptions import CornerDetectionError
 from torchvision.transforms import functional as ft
 
 params_path = path.join(path.dirname(__file__), '../models/parameters/interpreter.pmt')
@@ -14,7 +15,10 @@ class BoardInterpreter():
 
     def __call__(self, image):
         x = ft.to_tensor(image).unsqueeze(0)
-        probabilities = self.cnn(x).detach().squeeze().numpy()
+        probabilities, error_prob = self.cnn(x)
+        probabilities = probabilities.detach().squeeze().numpy()
+        if error_prob > 0.5: 
+            raise CornerDetectionError()
         configuration = probabilities.argmax(axis=0) - 1
         probabilities = np.exp(probabilities.max(axis=0)) / np.exp(probabilities).sum(axis=0)
         return configuration, probabilities
