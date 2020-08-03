514107a016e4dbed9419da070703af549b5417fb
cyones <cristianyones@argeniss.com>
2020-08-03 12:52:36 -0300

    Corner detection sensibility increased

--- src/corner_detector.py
+++ src/corner_detector.py
@@ -5,8 +5,8 @@ import torch
 from torchvision.transforms import functional as ft
 from scipy import ndimage
 
-from .exceptions import NoBoardError, BoardTooFarError, MissingCornerError
-from ..models import Segmenter
+from src.exceptions import NoBoardError, BoardTooFarError, MissingCornerError
+from models import Segmenter
 
 params_path = path.join(path.dirname(__file__), '../models/parameters/segmenter.pmt')
 
@@ -25,38 +25,37 @@ class CornerDetector():
             tensor = tensor.cuda()
         segmentation = self.unet(tensor)
         segmentation = segmentation.detach().cpu().numpy().squeeze()
-        segmentation[segmentation < 0.1] = 0.0
 
-        ccomponent, ncomponent = ndimage.label(segmentation[0])
-        if ncomponent < 1:
-            raise NoBoardError
+        segmentation_board = segmentation[0]
+        segmentation_board[segmentation_board < 1e-2] = 0.0
+
+        ccomponent, ncomponent = ndimage.label(segmentation_board)
+        if ncomponent < 1: raise NoBoardError()
 
         greather_component, component_size = stats.mode(ccomponent[ccomponent>0], axis=None)
-        if component_size < 100:
-            raise NoBoardError
-        if component_size < 3072: 
-            raise BoardTooFarError
+        if component_size < 100: raise NoBoardError()
+        if component_size < 3072: raise BoardTooFarError()
 
-        segmentation = segmentation[2]
-        segmentation[ccomponent != greather_component] = 0.0
-        
-        ccomponent, ncomponent = ndimage.label(segmentation)
-        if ncomponent < 4:
-            raise MissingCornerError(ncomponent)
+        segmentation_corner = segmentation[2]
+        segmentation_corner[ccomponent != greather_component] = 0.0
+        segmentation_corner[segmentation_corner < 1e-3] = 0.0
+
+        ccomponent, ncomponent = ndimage.label(segmentation_corner)
+        if ncomponent < 4: raise MissingCornerError(ncomponent)
 
         confidence = np.zeros((4))
         vertexs = -np.ones((4, 2))
         for i in range(4):
-            max_probability = segmentation.max()
+            max_probability = segmentation_corner.max()
             confidence[i] = max_probability
-            max_position = np.where(segmentation == max_probability)
+            max_position = np.where(segmentation_corner == max_probability)
 
             mask = ccomponent[max_position[0][0], max_position[1][0]] == ccomponent
             p = np.where(mask)
-            w = segmentation[mask]
+            w = segmentation_corner[mask]
             w /= w.sum()
             vertexs[i] = np.array([(p[0] * w).sum(), (p[1] * w).sum()])
-            segmentation[mask] = 0.0
+            segmentation_corner[mask] = 0.0
         vertexs = 2 * vertexs[:,[1,0]]
         idxs = self.order_vertexs(vertexs, image.size)
         return vertexs[idxs], confidence[idxs] 
