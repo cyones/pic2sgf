90a8f8ae788658d95010a998e419233a34e668d2
cyones <cristianyones@argeniss.com>
2020-08-03 12:48:30 -0300

    Accept images of any size

--- pic2array.py
+++ pic2array.py
@@ -1,5 +1,6 @@
-from .src import CornerDetector, BoardExtractor, BoardInterpreter, BoardSizer
+from src import CornerDetector, BoardExtractor, BoardInterpreter, BoardSizer
 from PIL import Image
+from PIL.ImageOps import expand
 from time import time
 import torch as tr
 import numpy as np
@@ -11,33 +12,33 @@ class Pic2Array():
         tr.set_num_threads(num_threads)
         self.corner_detector = CornerDetector()
         self.board_sizer = BoardSizer()
-        self.board_extractor = { 9: BoardExtractor(9),
-                                13: BoardExtractor(13),
-                                19: BoardExtractor(19)}
+        self.board_extractor = { 9: BoardExtractor( 9 * 16),
+                                13: BoardExtractor(13 * 16),
+                                19: BoardExtractor(19 * 16)}
         self.board_interpreter = BoardInterpreter()
 
+    def resize(self, image, size):
+        new_image= Image.new(image.mode, size)
+        image.thumbnail(size, Image.LANCZOS)
+        x_offset= (new_image.size[0] - image.size[0]) // 2
+        y_offset= (new_image.size[1] - image.size[1]) // 2
+        new_image.paste(image, (x_offset, y_offset))
+        return new_image
 
     def __call__(self, image):
         start = time()
-        landscape = False
         if image.size[0] > image.size[1]:
-            image = image.resize((512, 384), resample=Image.LANCZOS)
-        elif image.size[0] < image.size[1]:
-            landscape = True
-            image = image.resize((384, 512), resample=Image.LANCZOS).transpose(Image.ROTATE_90)
+            image = self.resize(image, (512, 384))
+        elif image.size[1] > image.size[0]:
+            image = self.resize(image, (384, 512))
         else:
-            image = image.resize((384, 384), resample=Image.LANCZOS)
-
+            image = image.resize((384,384), resample=Image.LANCZOS)
 
         corner_prediction, corner_confidence = self.corner_detector(image)
         size_prediction, size_confidence = self.board_sizer(image, corner_prediction)
         board_image, board_tilt = self.board_extractor[size_prediction](image, corner_prediction)
         board_image = board_image.transpose(Image.ROTATE_180)
         configuration_prediction, configuration_confidence = self.board_interpreter(board_image)
-        
-        if landscape:
-            configuration_prediction = np.rot90(configuration_prediction, k=3)
-            board_image = board_image.transpose(Image.ROTATE_270)
         
         report = {
             'corner_prediction' : corner_prediction,
