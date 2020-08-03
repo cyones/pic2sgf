91dd32931c5349645f45484fb13e40b9babf7fb0
cyones <cristianyones@argeniss.com>
2020-08-03 12:52:16 -0300

    Error detection moved from interpreter to sizer

--- src/board_interpreter.py
+++ src/board_interpreter.py
@@ -1,7 +1,6 @@
 from os import path
 import numpy as np
-from ..models import Interpreter
-from .exceptions import CornerDetectionError
+from models import Interpreter
 from torchvision.transforms import functional as ft
 
 params_path = path.join(path.dirname(__file__), '../models/parameters/interpreter.pmt')
@@ -15,10 +14,10 @@ class BoardInterpreter():
 
     def __call__(self, image):
         x = ft.to_tensor(image).unsqueeze(0)
-        probabilities, error_prob = self.cnn(x)
+        probabilities, _ = self.cnn(x)
+
         probabilities = probabilities.detach().squeeze().numpy()
-        if error_prob > 0.5: 
-            raise CornerDetectionError()
         configuration = probabilities.argmax(axis=0) - 1
         probabilities = np.exp(probabilities.max(axis=0)) / np.exp(probabilities).sum(axis=0)
+        
         return configuration, probabilities
--- src/board_sizer.py
+++ src/board_sizer.py
@@ -1,9 +1,10 @@
 from os import path
 import numpy as np
-from pic2sgf.models import Sizer
+from models import Sizer
+from src.exceptions import CornerDetectionError
 from torchvision.transforms import functional as ft
 
-from pic2sgf.src import BoardExtractor
+from src import BoardExtractor
 
 params_path = path.join(path.dirname(__file__), '../models/parameters/sizer.pmt')
 
@@ -14,11 +15,16 @@ class BoardSizer():
         self.cnn.load(params_path)
         self.cnn.eval()
         self.classes = [9, 13, 19]
-        self.board_extractor = BoardExtractor(8)
+        self.board_extractor = BoardExtractor(192)
 
     def __call__(self, image, corners):
         image, _ = self.board_extractor(image, corners)
         x = ft.to_tensor(image).mean(0, keepdim=True).unsqueeze(0)
-        probabilities = self.cnn(x).detach().numpy()
-        board_size = probabilities.argmax()
-        return self.classes[board_size], probabilities
+
+        probabilities = self.cnn(x).squeeze()
+        probabilities[3] *= 1e-3
+
+        class_idx = probabilities.detach().numpy().argmax()
+        if class_idx == 3: raise CornerDetectionError()
+
+        return self.classes[class_idx], probabilities[:2]
