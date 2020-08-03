e4898e1770b75549e7417e145e43bd5c57162251
cyones <cristianyones@argeniss.com>
2020-08-03 12:50:39 -0300

    Segmenter improved with GELU and bigger model

--- models/segmenter.py
+++ models/segmenter.py
@@ -1,27 +1,19 @@
 import torch as tr
 from torch import nn
-from torchvision import transforms
+from .iblock import iblock
 
-class iblock(nn.Module):
-    def __init__(self, dims):
-        super(iblock, self).__init__()
-        self.conv_path = nn.Sequential(nn.BatchNorm2d(dims), nn.ELU(),
-                                       nn.Conv2d(dims, dims, kernel_size=3, padding=1),
-                                       nn.ELU(), nn.BatchNorm2d(dims),
-                                       nn.Conv2d(dims, dims, kernel_size=3, padding=1))
-        
-    def forward(self, x):
-        return x + self.conv_path(x)
 
 def Pooling(in_dim, out_dim):
-    return nn.Sequential(nn.BatchNorm2d(in_dim), nn.ELU(),
+    return nn.Sequential(nn.BatchNorm2d(in_dim), nn.GELU(),
                          nn.Conv2d(in_dim, out_dim, kernel_size=2, stride=2))
+
 
 class Segmenter(nn.Module):
     def __init__(self):
         super(Segmenter, self).__init__()
-        self.downscale = nn.ModuleList([Pooling(8, 16), Pooling(16, 32), Pooling(32, 64)])
+        self.downscale = nn.ModuleList([Pooling(8, 16), Pooling(16, 32), Pooling(32, 64), Pooling(64, 96)])
         self.upscale = nn.ModuleList([nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+                                      nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
                                       nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
                                       nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)])
         
@@ -29,21 +21,25 @@ class Segmenter(nn.Module):
 
         self.in_cnn = nn.ModuleList([nn.Sequential(iblock( 8), iblock( 8)),
                                      nn.Sequential(iblock(16), iblock(16)),
-                                     nn.Sequential(iblock(32), iblock(32))
+                                     nn.Sequential(iblock(32), iblock(32)),
+                                     nn.Sequential(iblock(64), iblock(64))
                                      ])
 
-        self.bottom = nn.Sequential(iblock(64), iblock(64),
-                                    nn.BatchNorm2d(64), nn.ELU(),
-                                    nn.Conv2d(64, 32, kernel_size=1))
+        self.bottom = nn.Sequential(iblock(96), iblock(96),
+                                    nn.BatchNorm2d(96), nn.GELU(),
+                                    nn.Conv2d(96, 64, kernel_size=1))
 
-        self.out_cnn = nn.ModuleList([nn.Sequential(iblock(32), iblock(32),
-                                                    nn.ELU(), nn.BatchNorm2d(32),
+        self.out_cnn = nn.ModuleList([nn.Sequential(iblock(64), iblock(64),
+                                                    nn.GELU(), nn.BatchNorm2d(64),
+                                                    nn.Conv2d(64, 32, kernel_size=1)),
+                                      nn.Sequential(iblock(32), iblock(32),
+                                                    nn.GELU(), nn.BatchNorm2d(32),
                                                     nn.Conv2d(32, 16, kernel_size=1)),
                                       nn.Sequential(iblock(16), iblock(16),
-                                                    nn.ELU(), nn.BatchNorm2d(16),
+                                                    nn.GELU(), nn.BatchNorm2d(16),
                                                     nn.Conv2d(16, 8, kernel_size=1)),
                                       nn.Sequential(iblock(8), iblock(8),
-                                                    nn.ELU(), nn.BatchNorm2d(8))
+                                                    nn.GELU(), nn.BatchNorm2d(8))
         ])
             
         self.last_cnn = nn.Sequential(nn.Conv2d(8, 3, kernel_size=1), nn.Sigmoid())
