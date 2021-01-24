8139319fe59e4f790772ffd33f953767cb3df1a5
cyones <cristianyones@argeniss.com>
2021-01-24 00:51:55 -0300

    Updated models

--- models/interpreter.py
+++ models/interpreter.py
@@ -1,7 +1,17 @@
 import torch as tr
 from torch import nn
-from .iblock import iblock
-from .global_features import GlobalFeatures
+
+
+class iblock(nn.Module):
+    def __init__(self, dims, ksize=3):
+        super(iblock, self).__init__()
+        self.conv_path = nn.Sequential(nn.BatchNorm2d(dims), nn.GELU(), nn.Dropout2d(0.1),
+                                       nn.Conv2d(dims, dims, kernel_size=ksize, padding=ksize // 2),
+                                       nn.GELU(), nn.BatchNorm2d(dims),
+                                       nn.Conv2d(dims, dims, kernel_size=ksize, padding=ksize // 2))
+        
+    def forward(self, x):
+        return x + self.conv_path(x)
 
 
 def Pooling(in_dim, out_dim):
@@ -17,19 +27,17 @@ class Interpreter(nn.Module):
             nn.Conv2d(3, 32, kernel_size=3, stride=3),
             nn.GELU(), nn.BatchNorm2d(32),
             nn.Conv2d(32, 12, kernel_size=3, padding=1),
-            iblock(12), iblock(12), iblock(12), iblock(12),
+            iblock(12), iblock(12), iblock(12),
             Pooling(12, 24), 
-            iblock(24), iblock(24), iblock(24), iblock(24),
+            iblock(24), iblock(24), iblock(24),
             Pooling(24, 48), 
-            iblock(48), iblock(48), iblock(48), iblock(48),
+            iblock(48), iblock(48), iblock(48),
             Pooling(48, 96),
-            iblock(96), iblock(96), iblock(96), iblock(96),
+            iblock(96, ksize=1), iblock(96, ksize=1), iblock(96, ksize=1),
             nn.GELU(), nn.BatchNorm2d(96),
             )
         self.displacement = nn.Sequential(
-            nn.Conv2d(96, 24, kernel_size=1),
-            nn.GELU(), nn.BatchNorm2d(24),
-            nn.Conv2d(24, 2, kernel_size=1)
+            nn.Conv2d(96, 2, kernel_size=1)
         )
         self.position = nn.Sequential(
             nn.Conv2d(96, 24, kernel_size=1),
@@ -37,7 +45,7 @@ class Interpreter(nn.Module):
             nn.Conv2d(24, 3, kernel_size=1)
         )
         self.wrong = nn.Sequential(
-            nn.Conv2d(96, 24, kernel_size=1),
+            nn.Conv2d(96, 24, kernel_size=3, padding=1),
             nn.GELU(), nn.BatchNorm2d(24),
             nn.Conv2d(24, 1, kernel_size=1),
             nn.AdaptiveMaxPool2d(1),
@@ -47,9 +55,8 @@ class Interpreter(nn.Module):
     def forward(self, x):
         x = self.conv_blocks(x)
         position = self.position(x)
-        displacement = self.displacement(x)
         wrong = self.wrong(x).view(-1, 1)
-        return position, displacement, wrong
+        return position, wrong
       
     def load(self, fname):
         self.load_state_dict(tr.load(fname, map_location=lambda storage, loc: storage))
