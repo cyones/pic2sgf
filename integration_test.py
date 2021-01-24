4e8771ca53a102121cede0df85b460ed82ed1350
cyones <cristianyones@argeniss.com>
2021-01-24 00:51:07 -0300

    Moved test scripts

--- integration_test.py
+++ integration_test.py
@@ -1,18 +1,19 @@
 import os
 from PIL import Image
-from pic2sgf import Pic2Array
+from pic2array import Pic2Array
 from matplotlib import pyplot as plt
 from matplotlib.colors import Normalize
 
 interpreter = Pic2Array(4)
 
-fn = "./testing/fotos-desde-reddit/22cd4c8f2529b9d05dfd147bf2fe0995.jpg"
+fn = "./testing/fotos-desde-reddit/r (16).jpg"
 img = Image.open(fn)
 fig = plt.figure()
 plt.subplot(1, 2, 1)
 plt.imshow(img)
 try:
     board, report = interpreter(img)
+    print(report)
 except Exception as e:
     plt.suptitle(f"Failed: {e}")
     print(e)
--- integration_test_2.py
+++ integration_test_2.py
@@ -4,7 +4,7 @@ from pic2sgf import Pic2Array
 
 interpreter = Pic2Array(4)
 
-fn = "pic2sgf/testing/fotos-desde-reddit/22cd4c8f2529b9d05dfd147bf2fe0995.jpg"
+fn = "pic2sgf/testing/fotos-desde-reddit/r (16).jpg"
 img = Image.open(fn)
 try:
     board, report = interpreter(img)
